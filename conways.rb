886cc6757f8fcbc6ec1f27b1eaea08da41301669
fractalcactus <8527715+fractalcactus@users.noreply.github.com>
2021-05-20 19:38:35 +1200

    WIP: - begin understanding and implementing Conway's Game of life - implement a few core methods for the game of life, and test them - implement these methods as top level, purely functional methods while I get the tests passing - implement the methods with the goal of passing the tests, not optimising the code - figure out how to print an 'animation' of the game to the command line, this code is commented out for future reference

---
+++ conways.rb
@@ -0,0 +1,184 @@
+require 'pry'
+# system("clear") || system("cls")
+# puts "conways is starting"
+# sleep(1)
+# system("clear") || system("cls")
+# print 'x'
+# print 'x'
+# print 'x'
+# print 'x'
+# print 'x'
+# print 'x'
+# print "\n"
+# print 'x'
+# print 'x'
+# print 'x'
+# print 'x'
+# print 'x'
+# print 'x'
+# print "\n"
+# sleep(1)
+# system("clear") || system("cls")
+# print '0'
+# print '0'
+# print '0'
+# print '0'
+# print '0'
+# print '0'
+# print "\n"
+# print '0'
+# print '0'
+# print '0'
+# print '0'
+# print '0'
+# print '0'
+# print "\n"
+# sleep(1)
+# system("clear") || system("cls")
+# print 'x'
+# print 'x'
+# print 'x'
+# print 'x'
+# print 'x'
+# print 'x'
+# print "\n"
+# print 'x'
+# print 'x'
+# print 'x'
+# print 'x'
+# print 'x'
+# print 'x'
+# print "\n"
+# sleep(1)
+# system("clear") || system("cls")
+# print '0'
+# print '0'
+# print '0'
+# print '0'
+# print '0'
+# print '0'
+# print "\n"
+# print '0'
+# print '0'
+# print '0'
+# print '0'
+# print '0'
+# print '0'
+# print "\n"
+# sleep(1)
+# system("clear") || system("cls")
+# puts "conways ended"
+
+# -------------------Seeds-------------------
+# a 'seed' is the starting pattern for the game
+BLINKER = [
+           [0,0,0,0,0],
+           [0,0,1,0,0],
+           [0,0,1,0,0],
+           [0,0,1,0,0],
+           [0,0,0,0,0]
+          ]
+TOAD = [
+[0,0,0,0,0,0],
+[0,0,0,0,0,0],
+[0,0,1,1,1,0],
+[0,1,1,1,0,0],
+[0,0,0,0,0,0],
+[0,0,0,0,0,0]
+]
+
+
+#### 1 parameter
+# - pattern, a 2D array, representing rows and columns
+### output
+# - prints out a 2D array to the command line
+def print_pattern(pattern)
+  pattern.each do |row|
+    row.each do |col|
+      print col
+    end
+    print "\n"
+  end
+end
+
+#### 3 paramters
+# 1. row, integer representing the row coordinate of a cell in the pattern
+# 2. col, integer representing the column coordinate of a cell in the pattern
+# 3. patttern, a 2D array
+#### output
+# returns an integer representing the total of a cell's neighbors
+# a cell can have up to eight neighbors.
+# a neighbor is a cell that is 'alive', i.e contains a 1, and
+# is horizontally, vertically, or diagonally adjacent
+def count_neighbours(row, col, pattern)
+ neighbors = 0
+ coordinates_of_surrounding_cells = get_coordinates_of_surrounding_cells(row, col, pattern)
+ # for each coordinate in coordinates_of_surrounding_cells,
+ # increment neighbors if pattern contains a 1 at that coordinate
+ # the coordinates are in the form [row,col]
+ coordinates_of_surrounding_cells.each do |coord|
+  if (row == 5 && col == 0)
+    # binding.pry
+  end
+  surrounding_cell_row = coord[0]
+  surrounding_cell_col = coord[1]
+  if pattern[surrounding_cell_row][surrounding_cell_col] == 1
+    neighbors +=1
+  end
+ end
+ neighbors
+end
+
+#### 3 paramters
+# 1. row, integer representing the row coordinate of a cell in the pattern
+# 2. col, integer representing the column coordinate of a cell in the pattern
+# 3. patttern, a 2D array
+###  output
+# returns an array of coordinate arrays in the form [row,col]
+# TODO: these are not neighbours. a neighbor is alive. these are surrounding
+def get_coordinates_of_surrounding_cells (row, col, pattern)
+  neighbors = []
+  # get the coordinates of cells that are horizontally, vertically, or diagonally adjacent
+  # to the parameters row, col
+  # this will include invalid negative indices
+  # TODO: optimise this to not bother checking out of bound indices
+  neighbors << [row - 1, col - 1]
+  neighbors << [row - 1, col]
+  neighbors << [row - 1, col + 1]
+  neighbors << [row, col - 1]
+  neighbors << [row, col + 1]
+  neighbors << [row + 1, col - 1]
+  neighbors << [row + 1, col]
+  neighbors << [row + 1, col + 1];
+  # remove invalid negative indices,
+  neighbors.reject!{|neighbor|
+    neighbor.any?{|coord| coord < 0}
+  }
+  # remove coordinates outside the bounds of the pattern
+  neighbors.reject!{|neighbor|
+    neighbor.any?{|coord| (coord[0] > pattern.length) || (coord[1] > pattern[2].length)}
+}
+neighbors
+end
+#### 3 paramters
+# 1. row, integer representing the row coordinate of a cell in the pattern
+# 2. col, integer representing the column coordinate of a cell in the pattern
+# 3. patttern, a 2D array
+###  output
+# returns a 1 or 0, 1 represents an alive cell, 0 represents a dead cell
+def update_cell_value(row, col, pattern)
+  # Any live cell with two or three live neighbours survives.
+  # Any dead cell with three live neighbours becomes a live cell.
+  # All other live cells die in the next generation. Similarly, all other dead cells stay dead.
+  is_alive = pattern[row][col] == 1
+  total_neighbors = count_neighbours(row, col, pattern)
+  if is_alive && (total_neighbors == 2 || total_neighbors == 3)
+    return 1
+  end
+
+  if !is_alive & (total_neighbors == 3)
+    return 1
+  end
+
+  return 0
+end
---
+++ conways_tests.rb
@@ -0,0 +1,93 @@
+require_relative "conways"
+require "test/unit"
+
+class TestConways < Test::Unit::TestCase
+  def test_get_coordinates_of_neighbors
+    surrounding_cells_for_row_1_col_2= [
+      [0,1],[0,2],[0,3],
+      [1,1],[1,3],
+      [2,1],[2,2],[2,3]
+    ]
+    assert_equal(surrounding_cells_for_row_1_col_2, get_coordinates_of_surrounding_cells(1,2,BLINKER) )
+
+    surrounding_cells_for_row_0_col_0 = [
+      [0,1],
+      [1,0],
+      [1,1]
+    ]
+    assert_equal(surrounding_cells_for_row_0_col_0, get_coordinates_of_surrounding_cells(0,0,BLINKER) )
+  end
+  def test_count_neighbors
+    # alive cells in BLINKER
+    assert_equal(1, count_neighbours(1,2,BLINKER))
+    assert_equal(2, count_neighbours(2,2,BLINKER))
+    assert_equal(1, count_neighbours(3,2,BLINKER))
+    # dead cells in BlINKER
+    assert_equal(3, count_neighbours(2,1,BLINKER))
+    assert_equal(3, count_neighbours(2,3,BLINKER))
+    # alive cells in TOAD
+    assert_equal(4, count_neighbours(2,2,TOAD))
+    assert_equal(4, count_neighbours(2,3,TOAD))
+    assert_equal(2, count_neighbours(2,4,TOAD))
+    assert_equal(2, count_neighbours(3,1,TOAD))
+    assert_equal(4, count_neighbours(3,2,TOAD))
+    assert_equal(4, count_neighbours(3,3,TOAD))
+    # dead cells in TOAD
+    assert_equal(0, count_neighbours(0,0,TOAD))
+    assert_equal(0, count_neighbours(0,1,TOAD))
+    assert_equal(0, count_neighbours(0,2,TOAD))
+    assert_equal(0, count_neighbours(0,3,TOAD))
+    assert_equal(0, count_neighbours(0,4,TOAD))
+    assert_equal(0, count_neighbours(0,5,TOAD))
+    assert_equal(1, count_neighbours(1,1,TOAD))
+    assert_equal(2, count_neighbours(1,2,TOAD))
+    assert_equal(3, count_neighbours(1,3,TOAD))
+    assert_equal(2, count_neighbours(1,4,TOAD))
+  end
+
+  def test_update_cell_value
+    # BLINKER
+    assert_equal(0, update_cell_value(1,2,BLINKER))
+    assert_equal(1, update_cell_value(2,2,BLINKER))
+    assert_equal(0, update_cell_value(3,2,BLINKER))
+    assert_equal(1, update_cell_value(2,1,BLINKER))
+    assert_equal(1, update_cell_value(2,3,BLINKER))
+    # TOAD
+    assert_equal(0, update_cell_value(0,0,TOAD))
+    assert_equal(0, update_cell_value(0,1,TOAD))
+    assert_equal(0, update_cell_value(0,2,TOAD))
+    assert_equal(0, update_cell_value(0,3,TOAD))
+    assert_equal(0, update_cell_value(0,4,TOAD))
+    assert_equal(0, update_cell_value(0,5,TOAD))
+    assert_equal(0, update_cell_value(1,0,TOAD))
+    assert_equal(0, update_cell_value(1,1,TOAD))
+    assert_equal(0, update_cell_value(1,2,TOAD))
+    assert_equal(1, update_cell_value(1,3,TOAD))
+    assert_equal(0, update_cell_value(1,4,TOAD))
+    assert_equal(0, update_cell_value(1,5,TOAD))
+    assert_equal(0, update_cell_value(2,0,TOAD))
+    assert_equal(1, update_cell_value(2,1,TOAD))
+    assert_equal(0, update_cell_value(2,2,TOAD))
+    assert_equal(0, update_cell_value(2,3,TOAD))
+    assert_equal(1, update_cell_value(2,4,TOAD))
+    assert_equal(0, update_cell_value(2,5,TOAD))
+    assert_equal(0, update_cell_value(3,0,TOAD))
+    assert_equal(1, update_cell_value(3,1,TOAD))
+    assert_equal(0, update_cell_value(3,2,TOAD))
+    assert_equal(0, update_cell_value(3,3,TOAD))
+    assert_equal(1, update_cell_value(3,4,TOAD))
+    assert_equal(0, update_cell_value(3,5,TOAD))
+    assert_equal(0, update_cell_value(4,0,TOAD))
+    assert_equal(0, update_cell_value(4,1,TOAD))
+    assert_equal(1, update_cell_value(4,2,TOAD))
+    assert_equal(0, update_cell_value(4,3,TOAD))
+    assert_equal(0, update_cell_value(4,4,TOAD))
+    assert_equal(0, update_cell_value(4,5,TOAD))
+    assert_equal(0, update_cell_value(5,0,TOAD))
+    assert_equal(0, update_cell_value(5,1,TOAD))
+    assert_equal(0, update_cell_value(5,2,TOAD))
+    assert_equal(0, update_cell_value(5,3,TOAD))
+    assert_equal(0, update_cell_value(5,4,TOAD))
+    assert_equal(0, update_cell_value(5,5,TOAD))
+ end
+end
