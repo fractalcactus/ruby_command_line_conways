d95f9af3e875bf90d4fd4961118a11403d19c133
fractalcactus <8527715+fractalcactus@users.noreply.github.com>
2021-05-29 20:53:28 +1200

    WIP: passing tests

--- conways.rb
+++ conways.rb
@@ -1,8 +1,12 @@
-require 'pry'
+# require 'pry'
 # system("clear") || system("cls")
 # puts "conways is starting"
 # sleep(1)
 # system("clear") || system("cls")
+# print_pattern(BLINKER)
+# sleep(1)
+# system("clear") || system("cls")
+# print_pattern
 # print 'x'
 # print 'x'
 # print 'x'
@@ -69,6 +73,8 @@ require 'pry'
 # system("clear") || system("cls")
 # puts "conways ended"
 
+
+
 # -------------------Seeds-------------------
 # a 'seed' is the starting pattern for the game
 BLINKER = [
@@ -87,6 +93,40 @@ TOAD = [
 [0,0,0,0,0,0]
 ]
 
+#### 1 parameter
+# - pattern, a 2D array, representing rows and columns
+### output
+# - a 2D array, representing rows and cols, the next click of conways
+def click(pattern)
+  next_pattern = Array.new(pattern.length) { Array.new(pattern.length){nil} }
+  # next_pattern =  [
+  #   ["first","first","first","first","first","first"],
+  #   ["second","second","second","second","second","second"],
+  #   ["third","third","third","third","third","third"],
+  #   ["fourth","fourth","fourth","fourth","fourth","fourth"],
+  #   ["fith","fith","fith","fith","fith","fith",],
+  #   ["sixth","sixth","sixth","sixth","sixth","sixth"]
+  #   ]
+  puts "next_pattern starts as an 2D array filled with 'hi'"
+  print_pattern(next_pattern)
+  puts ''
+  print next_pattern
+  puts ''
+  count_of_calls_to_update_cell_value = 0
+  pattern.each_with_index do |row, row_index|
+    row.each_with_index do |col, col_index|
+      puts "at (row,col) (#{row_index},#{col_index}), update_cell_value returns: #{update_cell_value(row_index,col_index,pattern)}"
+      puts "next_pattern at [#{row_index}][#{col_index}] is initally #{next_pattern[row_index][col_index]}"
+      next_pattern[row_index][col_index] = update_cell_value(row_index,col_index,pattern)
+      puts "next_pattern at [#{row_index}][#{col_index}] is now #{next_pattern[row_index][col_index]}"
+      count_of_calls_to_update_cell_value += 1
+    end
+  end
+  puts "so after all that, tell me, next_pattern[3][4] == 1? answer: #{next_pattern[3][4]}"
+  puts "there were #{count_of_calls_to_update_cell_value} calls to update_cell_value"
+  print_pattern(next_pattern)
+  next_pattern
+end
 
 #### 1 parameter
 # - pattern, a 2D array, representing rows and columns
@@ -117,9 +157,6 @@ def count_neighbours(row, col, pattern)
  # increment neighbors if pattern contains a 1 at that coordinate
  # the coordinates are in the form [row,col]
  coordinates_of_surrounding_cells.each do |coord|
-  if (row == 5 && col == 0)
-    # binding.pry
-  end
   surrounding_cell_row = coord[0]
   surrounding_cell_col = coord[1]
   if pattern[surrounding_cell_row][surrounding_cell_col] == 1
@@ -151,14 +188,17 @@ def get_coordinates_of_surrounding_cells (row, col, pattern)
   neighbors << [row + 1, col]
   neighbors << [row + 1, col + 1];
   # remove invalid negative indices,
+
   neighbors.reject!{|neighbor|
     neighbor.any?{|coord| coord < 0}
   }
-  # remove coordinates outside the bounds of the pattern
+
+  # remove coordinates outside the bounds of the pattern, if row is greater than the zero indexed rows || col is greater than the zero indexed
   neighbors.reject!{|neighbor|
-    neighbor.any?{|coord| (coord[0] > pattern.length) || (coord[1] > pattern[2].length)}
-}
-neighbors
+   neighbor[0] >= pattern.length || neighbor[1] >= pattern.length
+  }
+
+  neighbors
 end
 #### 3 paramters
 # 1. row, integer representing the row coordinate of a cell in the pattern
@@ -182,3 +222,12 @@ def update_cell_value(row, col, pattern)
 
   return 0
 end
+
+
+# system("clear") || system("cls")
+# puts "conways is starting"
+# sleep(1)
+# system("clear") || system("cls")
+# print_pattern(BLINKER)
+# sleep(1)
+# system("clear") || system("cls")
