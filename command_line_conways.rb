bab21ee1e29045377556b830d73f33b590328b7d
fractalcactus <8527715+fractalcactus@users.noreply.github.com>
2021-06-06 11:59:32 +1200

    run rubocop

--- command_line_conways.rb
+++ command_line_conways.rb
@@ -1,5 +1,3 @@
-
-require 'pry-nav'
 # frozen_string_literal: true
 
 # A program to play Conway's Game of Life in the command line
@@ -112,31 +110,23 @@ class CommandLineConways
       surrounding_cells << [row_index - 1, col_index - 1]
     end
     # one row up, same col
-    unless top_edge_of_pattern?(row_index)
-      surrounding_cells << [row_index - 1, col_index]
-    end
+    surrounding_cells << [row_index - 1, col_index] unless top_edge_of_pattern?(row_index)
     # one row up, one col right
     unless top_edge_of_pattern?(row_index) || right_edge_of_pattern?(col_index)
       surrounding_cells << [row_index - 1, col_index + 1]
     end
     # same row, one col left
-    unless left_edge_of_pattern?(col_index)
-      surrounding_cells << [row_index, col_index - 1]
-    end
+    surrounding_cells << [row_index, col_index - 1] unless left_edge_of_pattern?(col_index)
     # same row, one col right
-    unless right_edge_of_pattern?(col_index)
-      surrounding_cells << [row_index, col_index + 1]
-    end
+    surrounding_cells << [row_index, col_index + 1] unless right_edge_of_pattern?(col_index)
     # one row down, one col left
     unless bottem_edge_of_pattern?(row_index) || left_edge_of_pattern?(col_index)
       surrounding_cells << [row_index + 1, col_index - 1]
     end
     # one row down, same col
-    unless bottem_edge_of_pattern?(row_index)
-      surrounding_cells << [row_index + 1, col_index]
-    end
+    surrounding_cells << [row_index + 1, col_index] unless bottem_edge_of_pattern?(row_index)
     # one row down, one col right
-    unless bottem_edge_of_pattern?(row_index) ||  right_edge_of_pattern?(col_index)
+    unless bottem_edge_of_pattern?(row_index) || right_edge_of_pattern?(col_index)
       surrounding_cells << [row_index + 1, col_index + 1]
     end
 
@@ -144,11 +134,11 @@ class CommandLineConways
   end
 
   def left_edge_of_pattern?(col_index)
-    col_index == 0
+    col_index.zero?
   end
 
   def top_edge_of_pattern?(row_index)
-    row_index ==  0
+    row_index.zero?
   end
 
   def right_edge_of_pattern?(col_index)
@@ -156,11 +146,15 @@ class CommandLineConways
   end
 
   def bottem_edge_of_pattern?(row_index)
-    row_index ==  @pattern_row_length_zero_indexed
+    row_index == @pattern_row_length_zero_indexed
   end
 
   def randomize_seed
-    Array.new(@pattern_row_length) { Array.new(@pattern_col_length) { [@dead_cell_graphic, @alive_cell_graphic].sample } }
+    Array.new(@pattern_row_length) do
+      Array.new(@pattern_col_length) do
+        [@dead_cell_graphic, @alive_cell_graphic].sample
+      end
+    end
   end
 
   def blinker_seed
@@ -261,8 +255,6 @@ end
   pattern_row_length: 150,
   pattern_col_length: 150,
   dead_cell_graphic: ' ',
-  alive_cell_graphic: '𓆦',
+  alive_cell_graphic: '𓆦'
 )
 @conways_game_random.run
-
-
